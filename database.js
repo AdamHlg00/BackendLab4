db867b1042d7ba6442dd062d0f0be31285e68359
AdamHlg00 <adamoliver.holgersson@gmail.com>
2023-03-24 17:08:01 +0100

    If user does not exist the program logs an error message and redirects back to the identify view

---
+++ database.js
@@ -0,0 +1,38 @@
+const sqlite3 = require('sqlite3').verbose()
+const db = new sqlite3.Database(':memory:')
+
+const userList = [
+  ['id1', 'user1', 'student', 'password'],
+  ['id2', 'user2', 'student', 'password2'],
+  ['id3', 'user3', 'teacher', 'password3'],
+  ['admin', 'admin', 'admin', 'admin']
+]
+
+db.serialize(() => {
+  db.run('CREATE TABLE Users (userID VARCHAR(255), name VARCHAR(255), role VARCHAR(255), password VARCHAR(255))')
+
+  userList.forEach((user) => {
+    let stmt = db.prepare('INSERT INTO Users (userID, name, role, password) VALUES (?, ?, ?, ?)')
+    stmt.run(user, (err) => {
+      if (err) {
+        throw err
+      }
+    })
+    stmt.finalize()
+  })
+})
+
+function getUser(userID) {
+  return new Promise((resolve, reject) => {
+    db.get('SELECT * FROM Users WHERE userID = ?', [userID], (err, rows) => {
+      if (!rows) {
+        let errorValue = 'User does not exist'
+        reject(errorValue)
+      } else {
+        resolve(rows)
+      }
+    })
+  })
+}
+
+module.exports = { db, getUser }
--- server.js
+++ server.js
@@ -7,27 +7,60 @@ app.set('view engine', 'ejs')
 app.use(express.urlencoded({ extended: false }))
 app.use(express.json())
 
+const db = require('./database')
+
 var currentKey = ''
 var currentPassword = ''
+let currentUser
 
 app.get('/', (req, res) => {
   res.redirect('/identify')
 })
 
-app.post('/identify', (req, res) => {
+app.post('/identify', async (req, res) => {
+  const userId = req.body.userId
   const username = req.body.password
   const token = jwt.sign(username, process.env.ACCESS_TOKEN_SECRET)
   currentKey = token
   currentPassword = username
-  res.redirect('/granted')
+
+  let user
+  db.getUser(userId)
+    .then((result) => {
+      user = result
+      console.log(user)
+      res.redirect('/granted')
+    })
+    .catch((error) => {
+      console.log(error)
+      res.redirect('/identify')
+    })
+
+  //console.log(currentPassword)
+  //console.log(currentKey)
 })
 
 app.get('/identify', (req, res) => {
   res.render('identify.ejs')
 })
 
-app.get('/granted', (req, res) => {
+function authenticateToken(req, res, next) {
+  if (currentKey == '') {
+    res.redirect('/identify')
+  } else if (jwt.verify(currentKey, process.env.ACCESS_TOKEN_SECRET)) {
+    //console.log(jwt.verify(currentKey, process.env.ACCESS_TOKEN_SECRET))
+    next()
+  } else {
+    res.redirect('/identify')
+  }
+}
+
+app.get('/granted', authenticateToken, (req, res) => {
   res.render('start.ejs')
+})
+
+app.get('/admin', (req, res) => {
+  res.render('admin.ejs')
 })
 
 app.listen(3000)
